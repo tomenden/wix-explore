7cc8594d375259fbefb4e2fed4dca61e5543e647
tome <tome@wix.com>
2015-12-17 16:29:07 +0200

    style updates

--- src/components/UserSite.js
+++ src/components/UserSite.js
@@ -10,26 +10,32 @@ export default class UserSite extends Component {
         let {thumbnailUrl, description, authorUserName, views, comments = [], likes = [], id:siteId} = this.props.site;
         let {userName, likeFunction} = this.props;
         let likeClasses = classNames({
-            'liked': _.includes(likes, userName)
+            'liked': _.includes(likes, userName),
+            'social-section': true
         });
         return (
             <div className="user-site">
                 <img className="site-thumbnail" src={thumbnailUrl} width="100%"/>
-                <div className="user-image-container">
-                    <img src="/src/images/userAvatar.svg" alt=""/>
-                </div>
+                <img src="/src/images/userAvatar2.svg" alt="" className="user-avatar"/>
+
                 <span>By {authorUserName}</span>
 
                 <div className="social-bar">
                     <div>
-                        <span onClick={()=>likeFunction(siteId)} className={likeClasses}>
+                        <div onClick={()=>likeFunction(siteId)} className={likeClasses}>
                             <img src="/src/images/like.svg" className="like-icon like-icon-empty"/>
                             <img src="/src/images/like-full.svg" className="like-icon like-icon-full"/>
-                        </span>
+                            {_.get(likes, 'length', 0)}
+                        </div>
 
-                        {_.get(likes, 'length', 0)}
-                        <img src="/src/images/views.svg"/>{views}
-                        <img src="/src/images/comments.svg"/> {_.get(comments, 'length', 0)}
+                        <div className="social-section">
+                            <img src="/src/images/views.svg"/>
+                            <span>{views}</span>
+                        </div>
+                        <div className="social-section">
+                            <img src="/src/images/comments.svg"/>
+                            <span>{_.get(comments, 'length', 0)}</span>
+                        </div>
 
                     </div>
                 </div>
