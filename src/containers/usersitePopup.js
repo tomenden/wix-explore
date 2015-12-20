6880564aa57cc2941237db872ee343a0a2646224
tome <tome@wix.com>
2015-12-20 10:43:17 +0200

    fixed merge conflicts

--- src/containers/usersitePopup.js
+++ src/containers/usersitePopup.js
@@ -8,7 +8,7 @@ import classNames from 'classnames';
 
 let UserSitePopup = (props) => {
     let likeClasses = classNames({
-        'liked': _.includes(props.site.likes, props.userName),
+        'liked': _.includes(_.get(props.site, 'likes', []), props.userName),
         'likes-counter': true
     });
 
@@ -28,115 +28,116 @@ let UserSitePopup = (props) => {
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
         return (
-            <div className="user-site-popup">
-                <div className="author-container">
-                    <div className="author-image">
-                        <img src={props.site.avatarUrl}/>
+            <div>
+                <div className="user-site-popup">
+                    <div className="author-container">
+                        <div className="author-image">
+                            <img src={props.site.avatarUrl}/>
 
-                </div>
-                <div className="author-name">
-                    {props.site.authorUserName}
-                </div>
-                <div className="author-more-info">
-                    {props.site.authorDescription}
-                </div>
-                <div className="author-message-btn">
-                    Message
-                </div>
-                <div className="author-more-sites">
-                    Made from this template
-                    <img src={props.site.originalTemplateUrl}/>
+                        </div>
+                        <div className="author-name">
+                            {props.site.authorUserName}
+                        </div>
+                        <div className="author-more-info">
+                            {props.site.authorDescription}
+                        </div>
+                        <div className="author-message-btn">
+                            Message
+                        </div>
+                        <div className="author-more-sites">
+                            Made from this template
+                            <img src={props.site.originalTemplateUrl}/>
 
-                    </div>
-
-            </div>
-            <div className="site-container">
-                <div className="site-thumbnail">
-                    <img src={props.site.thumbnailUrl} />
+                        </div>
 
                     </div>
-                    <div className="site-social-triplet">
-                </div>
-                <div className="site-social-triplet">
-                    <div className={likeClasses} onClick={()=>likeFunction(siteId)} >
-                        <img src="/src/images/like.svg" className="like-icon like-icon-empty"/>
-                        <img src="/src/images/like-full.svg" className="like-icon like-icon-full"/>
-                        {_.get(props.site.likes, 'length', 0)}
-                        <span>Likes</span>
+                    <div className="site-container">
+                        <div className="site-thumbnail">
+                            <img src={props.site.thumbnailUrl}/>
+
+                        </div>
+                        <div className="site-social-triplet">
+                        </div>
+                        <div className="site-social-triplet">
+                            <div className={likeClasses} onClick={()=>likeFunction(siteId)}>
+                                <img src="/src/images/like.svg" className="like-icon like-icon-empty"/>
+                                <img src="/src/images/like-full.svg" className="like-icon like-icon-full"/>
+                                {_.get(props.site.likes, 'length', 0)}
+                                <span>Likes</span>
+
+                            </div>
+                            <div className="views-counter">
+                                <img src="/src/images/views.svg"/>
+                                <span>{props.site.views}</span>
+                                <span>Views</span>
+                            </div>
+
+                        </div>
+                        <div className="site-comments">
+                            <div id="disqus_thread"></div>
+                        </div>
+                        <div className="site-comments">
+                            <div className="comments-counter">
+                                <span>Comments</span>
+                            </div>
+
+                            <div id="disqus_thread"></div>
+
+                        </div>
 
                     </div>
-                    <div className="views-counter">
-                        <img src="/src/images/views.svg"/>
-                        <span>{props.site.views}</span>
-                        <span>Views</span>
-                    </div>
+                    <div className="extra-container">
+                        <div className="site-name">
+                            {props.site.name}
 
-                    </div>
-                    <div className="site-comments">
-                        <div id="disqus_thread"></div>
-                </div>
-                <div className="site-comments">
-                    <div className="comments-counter">
-                        <span>Comments</span>
-                    </div>
+                        </div>
+                        <div className="site-description">
+                            {props.site.description}
 
-                    <div id="disqus_thread"></div>
+                        </div>
+                        <div className="site-category">
+                            Category
+                            <br/>
+                            {props.site.category}
 
-                    </div>
-
-                </div>
-                <div className="extra-container">
-                    <div className="site-name">
-                        {props.site.name}
-
-                    </div>
-                    <div className="site-description">
-                        {props.site.description}
-
+                        </div>
+                        <div className="social-share">
+                            Share
+                        </div>
+                        <div className="site-colors">
+                            Colors
+                        </div>
+                        <div className="site-tags">
+                            Tags
+                        </div>
                     </div>
                     <div className="site-category">
-                        Category
+                        <span className="sub-header">Category</span>
                         <br/>
-                        {props.site.category}
+                        <div>
+                            {props.site.category}
+                        </div>
+
+
+                    </div>
+                    <div className="site-tags">
+                        <span className="sub-header">Tags</span>
+
+                    </div>
+
+                    <div className="site-colors">
+                        <div className="sub-header">Colors</div>
+                        <div className="color-circle pink"></div>
+                        <div className="color-circle blue"></div>
+                        <div className="color-circle yellow"></div>
+                        <div className="color-circle black"></div>
+                        <div className="color-circle green"></div>
 
                     </div>
                     <div className="social-share">
-                        Share
+                        <span className="sub-header">Share It</span>
+
                     </div>
-                    <div className="site-colors">
-                        Colors
-                    </div>
-                    <div className="site-tags">
-                        Tags
-                    </div>
-                </div>
-                <div className="site-category">
-                    <span className="sub-header">Category</span>
-                    <br/>
-                    <div>
-                        {props.site.category}
-                    </div>
-
-
-                </div>
-                <div className="site-tags">
-                    <span className="sub-header">Tags</span>
-
-                </div>
-
-                <div className="site-colors">
-                    <div className="sub-header">Colors</div>
-                    <div className="color-circle pink"></div>
-                    <div className="color-circle blue"></div>
-                    <div className="color-circle yellow"></div>
-                    <div className="color-circle black"></div>
-                    <div className="color-circle green"></div>
-
-                </div>
-                <div className="social-share">
-                    <span className="sub-header">Share It</span>
-
-                </div>
 
                 </div>
 
