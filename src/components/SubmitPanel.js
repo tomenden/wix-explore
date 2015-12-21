6b2d3b383a655e32cdb1f0612dc566fe926deeb6
Tzabar Cohen <tzabarc@wix.com>
2015-12-21 15:35:10 +0200

    submitPanel first draft

---
+++ src/components/SubmitPanel.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+
+
+class SubmitPanel extends Component {
+    render() {
+        return (
+            <div className="">
+                <form action="">
+                    <input type="text" name="d"></input>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SubmitPanel;
